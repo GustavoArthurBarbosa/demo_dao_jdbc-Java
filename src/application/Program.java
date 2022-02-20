6401b5d1925ee99c7f445c0b19e097c2ea077b01
Your NameGustavoArthurBarbosa <gustavoarthurbarbosa@gmail.com>
2022-02-20 16:15:34 -0300

    Implementando método findById da classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,21 @@
 package application;
 
-import java.util.Date;
-
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Books");
-		
-		Seller seller = new Seller(22, "Bob", "bob@gmail.com", new Date(), 2300.00, department);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
-		System.out.println(department);
+		Seller seller = sellerDao.findById(2);
+		
 		System.out.println(seller);
+		
+		DB.closeConnection();
 	}
 
 }
